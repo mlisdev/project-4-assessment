69872f6fc20df46fe8abccc3a160810c69293ee9
mlisdev <srinehar@syr.edu>
2020-10-16 16:41:27 -0400

    Updates

---
+++ precache-manifest.6fe8a935a39f39045a21d2a55a66a742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82b3a0659ea362f96372ed211ae5f50",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "21c296707a21eddb184f",
+    "url": "/project-4-assessment/static/css/main.c4e726f8.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c296707a21eddb184f",
+    "url": "/project-4-assessment/static/js/main.007b7603.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
